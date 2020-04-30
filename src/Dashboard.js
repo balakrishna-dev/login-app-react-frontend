01529d0a94b83659abd6143c422989553831b20f
balakrishna-dev <60089627+balakrishna-dev@users.noreply.github.com>
2020-04-30 23:29:54 +0530

    Add files via upload

---
+++ src/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { getUser, removeUserSession } from './Utils/Common';
+import FormDialog from './material/FormDialog';
+
+
+function Dashboard(props) {
+  const user = getUser();
+
+  const handleLogout = () => {
+    removeUserSession();
+    props.history.push('/signin');
+  }
+ 
+  return (
+    <div>
+      Welcome {user.name}!<br /><br />
+      
+      <FormDialog /><br/><br/>
+      <input type="button" onClick={handleLogout} value="Logout" />
+    </div>
+  );
+}
+
+export default Dashboard;
