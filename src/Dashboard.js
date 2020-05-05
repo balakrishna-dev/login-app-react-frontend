1629672e1c42febc441ce3189c77d479a3648590
balakrishna-dev <balakrishna.v12@gmail.com>
2020-05-05 22:07:45 +0530

    material

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,26 +1,26 @@
-
 import React from 'react';
 import { getUser, removeUserSession } from './Utils/Common';
-import FormDialog, {handleClose} from './material/FormDialog';
-
+import FormDialog, { handleClose } from './material/FormDialog';
 
 function Dashboard(props) {
-  const user = getUser();
+	const user = getUser();
 
-  const handleLogout = () => {
-    removeUserSession();
-    props.history.push('/signin');
-  }
- 
-  return (
-    <div>
-      Welcome {user.name}!<br /><br />
-      
-      <FormDialog /><br/><br/>
-      <handleClose {...props.uname} />
-      <input type="button" onClick={handleLogout} value="Logout" />
-    </div>
-  );
+	const handleLogout = () => {
+		removeUserSession();
+		props.history.push('/signin');
+	};
+
+	return (
+		<div>
+			Welcome {user.name}!<br />
+			<br />
+			<FormDialog />
+			<br />
+			<br />
+			<handleClose {...props.uname} />
+			<input type="button" onClick={handleLogout} value="Logout" />
+		</div>
+	);
 }
 
 export default Dashboard;
